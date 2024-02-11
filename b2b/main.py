277fc330950ddd8e6cd88162913b96799993461f
s-kan <gkswltjr00@gmail.com>
2024-02-11 11:50:24 +0900

    decisiontree parameters

--- b2b/main.py
+++ b2b/main.py
@@ -3,11 +3,10 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-# main file!
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name} the sex machine')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name} the machine')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
